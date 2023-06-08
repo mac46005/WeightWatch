90fb3a0c868be611cadb59c954c16dd9b42359d2
Marco Preciado <mac46005@gmail.com>
2023-06-08 10:11:49 -0500

    Learning about how to make graphs using compose and some math

--- app/src/main/java/com/preciado/weightwatch/features/dashboard/DashboardView.kt
+++ app/src/main/java/com/preciado/weightwatch/features/dashboard/DashboardView.kt
@@ -1,8 +1,16 @@
 package com.preciado.weightwatch.features.dashboard
 
+import android.graphics.Paint
+import androidx.compose.animation.expandVertically
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -13,7 +21,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun DashBoardView(
@@ -23,15 +37,32 @@ fun DashBoardView(
     Column (modifier = modifier){
         
         //This will show a graph of all(or maybe from date to date) weight inputs
-        Box(modifier = Modifier){
+        Box(modifier = Modifier
+            .weight(2f)
+            .fillMaxWidth()){
             
         }
         //Shows relevant info such as weight average and eating habits
-        Card(modifier = Modifier) {
-            
+        Card(modifier = Modifier
+            .weight(2f)
+            .fillMaxWidth()
+            .padding(10.dp)) {
+            Column() {
+                Text(text = "Relevant Information yo")
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+                    Card() {
+                        Text(text = "Card 1")
+                    }
+                    Card() {
+                        Text(text = "Card 2")
+                    }
+                }
+            }
+
         }
         //Buttons to navigate to other views
         BottomAppBar(modifier = Modifier) {
+
             Button(onClick = { /*TODO*/ }) {
                 Text(text = "Log Weight")
             }
@@ -40,4 +71,33 @@ fun DashBoardView(
             }
         }
     }
+}
+
+
+@Composable
+fun Graph(
+    modifier: Modifier,
+    xValues: List<Int>,
+    yValues: List<Int>,
+    points: List<Float>,
+    paddingSpace: Dp,
+    VerticalStep:Int
+){
+    Box(
+        modifier = modifier
+            .background(Color.White)
+            .padding(horizontal = 8.dp, vertical = 12.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Canvas(modifier = Modifier.fillMaxWidth()){
+
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DashboardViewPreview(){
+    DashBoardView(navController = rememberNavController())
 }
